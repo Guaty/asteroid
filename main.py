00a6cc4e61fab2c48ae8ef41a72b6d66609f83c6
Guaty <bjarocha911@gmail.com>
2024-12-09 17:27:23 -0600

    add move to player, add player to updatable and drawable groups

--- main.py
+++ main.py
@@ -9,20 +9,31 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        for thing in updatable:
+            thing.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         
+        for thing in drawable:
+            thing.draw(screen)
+            
+        pygame.display.flip()
+
         #limit framerate to 60 fps
         dt = clock.tick(60) / 1000
     
